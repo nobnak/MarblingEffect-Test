d0e57ac0d3b438b94e57e006aa754d461031a771
Nakata Nobuyuki <nobu.nakata@gmail.com>
2022-12-01 02:06:59 +0900

    UVオフセットのテスト

---
+++ Assets/Scenes/TestOffset.cs
@@ -0,0 +1,26 @@
+using MarblingEffectSys;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestOffset : MonoBehaviour {
+
+    public Dependency dep = new Dependency();
+
+    public Texture offset_tex;
+
+    #region unity
+    private void Update() {
+        if (dep != null && dep.effect != null) {
+            dep.effect.OffsetTex = offset_tex;
+        }        
+    }
+    #endregion
+
+    #region declarations
+    [System.Serializable]
+    public class Dependency {
+        public MarblingPostEffect effect;
+    }
+    #endregion
+}
